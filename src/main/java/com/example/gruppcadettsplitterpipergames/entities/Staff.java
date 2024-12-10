767fb5b6d9076bb91f608eabb7a66025565a8605
BP <valkarian@valkarian.com>
2024-12-10 11:01:59 +0100

    Task: Added attributes/constructors/gets+sets to Staff entity

--- src/main/java/com/example/gruppcadettsplitterpipergames/entities/Staff.java
+++ src/main/java/com/example/gruppcadettsplitterpipergames/entities/Staff.java
@@ -12,5 +12,68 @@ public class Staff {
     @Column(name = "staff_id")
     private int id;
 
+    @Column(name = "staff_first_name", length = 20, nullable = false)
+    private String firstName;
 
+    @Column(name = "staff_last_name", length = 20, nullable = false)
+    private String lastName;
+
+    @Column(name = "staff_nick_name", length = 20, nullable = false)
+    private String nickName;
+
+//    @JoinColumn(name = "address_id")
+//    private Address address;
+
+    //Constructors
+
+    public Staff() {
+    }
+
+    public Staff(String firstName, String lastName, String nickName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+    }
+
+    //Getters & Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+//    public Address getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(Address address) {
+//        this.address = address;
+//    }
 }
