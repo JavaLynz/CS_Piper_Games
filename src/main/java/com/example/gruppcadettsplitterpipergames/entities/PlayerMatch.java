a46c6ec76b81746096a3c100f5e2f45bf03bebc6
JavaLynz <lynseyfoxstudent@gmail.com>
2024-12-10 09:17:13 +0100

    id added for all

--- src/main/java/com/example/gruppcadettsplitterpipergames/entities/PlayerMatch.java
+++ src/main/java/com/example/gruppcadettsplitterpipergames/entities/PlayerMatch.java
@@ -1,4 +1,14 @@
 package com.example.gruppcadettsplitterpipergames.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "player_matches")
+
 public class PlayerMatch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "player_match_id")
+    private int id;
 }
--- src/main/java/com/example/gruppcadettsplitterpipergames/entities/TeamMatch.java
+++ src/main/java/com/example/gruppcadettsplitterpipergames/entities/TeamMatch.java
@@ -1,4 +1,14 @@
 package com.example.gruppcadettsplitterpipergames.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "team_matches")
+
 public class TeamMatch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_match_id")
+    private int id;
 }
