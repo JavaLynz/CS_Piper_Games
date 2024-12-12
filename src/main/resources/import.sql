a6488dff1a6614d27b08d8b2e6f21b1a6cd49d41
BP <valkarian@valkarian.com>
2024-12-12 10:24:03 +0100

    Task: Updated import.sql to include staff import and address imports

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,10 @@
-INSERT INTO players(player_first_name, player_last_name, player_nick_name) VALUES
-(Harrison, Ford, Harry),
-(Keanu, Reeves, Keanu),
-(Alexander, Skarsgård, Alex),
-(Keira, Knightley,Keira),
-(Sandra, Bullock,Sandy);
+INSERT INTO addresses(address, district, city, postcode, country) VALUES ('9336 Civic Center Dr.', 'Beverly Hills','California', '90210', 'USA');
+INSERT INTO addresses(address, district, city, postcode, country) VALUES ('498 7th Ave.', 'New York','New York', '10018', 'USA');
+INSERT INTO addresses(address, district, city, postcode, country) VALUES ('Gruvbacken 4', '','Stockholm', '11634', 'Sweden');
+INSERT INTO addresses(address, district, city, postcode, country) VALUES ('Drury House, 34-43 Russell Street', '','London', 'WC2B 5HA', 'UK');
+INSERT INTO addresses(address, district, city, postcode, country) VALUES ('12-20 Lexington Street', '','London', 'W1F 0LE', 'UK');
+INSERT INTO addresses(address, district, city, postcode, country) VALUES ('916 West Burbank Bouvlevard', 'Burbank','California', '91506', 'USA');
+INSERT INTO addresses(address, district, city, postcode, country) VALUES ('485 West 46th St.', 'Hells Kitchen','New York', '10019', 'USA');
+
+INSERT INTO staff(staff_first_name, staff_last_name, staff_nick_name, address_id, staff_email) VALUES ("Benjamin", "Portsmouth", "Ben", 1, "b@p.com");
 
