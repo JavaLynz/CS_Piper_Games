7dc3ee75ede7a6c5d7b7c53ee48cd34623c7cc22
BP <valkarian@valkarian.com>
2024-12-16 16:35:10 +0100

    Fix: Altered TabMenu to no longer extend Application

--- src/main/java/com/example/gruppcadettsplitterpipergames/view/TabMenu.java
+++ src/main/java/com/example/gruppcadettsplitterpipergames/view/TabMenu.java
@@ -1,20 +1,16 @@
 package com.example.gruppcadettsplitterpipergames.view;
 
-import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
-import javafx.stage.Stage;
 
-public class TabMenu extends Application {
+public class TabMenu{
     private TabPane root = new TabPane();
     private StaffFX staffFX = new StaffFX(this.root);
 
-    @Override
-    public void start(Stage stage) throws Exception {
 
+    public Scene tabMenuScene(){
         this.root.getTabs().add(staffFX.createStaffTab());
-
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+        Scene scene = new Scene(this.root);
+        return scene;
     }
 }
