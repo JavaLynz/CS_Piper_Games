a0666c6f78fc1dd67899d377a91d98e8ed4c252e
BP <valkarian@valkarian.com>
2024-12-16 17:18:30 +0100

    Task: Added logout button to TabMenu

--- src/main/java/com/example/gruppcadettsplitterpipergames/view/TabMenu.java
+++ src/main/java/com/example/gruppcadettsplitterpipergames/view/TabMenu.java
@@ -1,15 +1,33 @@
 package com.example.gruppcadettsplitterpipergames.view;
 
+import com.example.gruppcadettsplitterpipergames.HelloApplication;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
 
 public class TabMenu{
     private TabPane root = new TabPane();
     private StaffFX staffFX = new StaffFX(this.root);
 
 
-    public Scene tabMenuScene(){
+    public Scene tabMenuScene(Stage stage, Scene firstScene){
         this.root.getTabs().add(staffFX.createStaffTab());
+        this.root.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
+
+        Button logoutBtn = new Button("Logout");
+        logoutBtn.setOnMouseClicked(mouseEvent -> {
+            stage.setScene(firstScene);
+        });
+
+
+        Tab logoutTab = new Tab();
+        logoutTab.setGraphic(logoutBtn);
+        logoutTab.setStyle("-fx-padding: 0; right:0");
+        this.root.getTabs().add(logoutTab);
+
         Scene scene = new Scene(this.root);
         return scene;
     }
