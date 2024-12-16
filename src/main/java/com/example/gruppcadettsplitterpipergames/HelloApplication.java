80a89d930c198a93f2ea6646591659960696fcf2
niklas-einarsson <n.einarsson@gmail.com>
2024-12-16 12:39:52 +0100

    Updated Team-class and added CRUD to TeamsDAO

--- src/main/java/com/example/gruppcadettsplitterpipergames/HelloApplication.java
+++ src/main/java/com/example/gruppcadettsplitterpipergames/HelloApplication.java
@@ -5,8 +5,12 @@ import com.example.gruppcadettsplitterpipergames.DAO.GamesDAO;
 import com.example.gruppcadettsplitterpipergames.DAO.PlayerDAO;
 import com.example.gruppcadettsplitterpipergames.DAO.TeamsDAO;
 import com.example.gruppcadettsplitterpipergames.entities.Player;
+import com.example.gruppcadettsplitterpipergames.view.PlayerFX;
+import com.example.gruppcadettsplitterpipergames.view.TeamFX;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -18,49 +22,56 @@ public class HelloApplication extends Application {
 
      //   Logger.getLogger("org.hibernate").setLevel(Level.OFF);
 
-    PlayerDAO playerDAO = new PlayerDAO();
-    AddressDAO addressDAO = new AddressDAO();
-    GamesDAO gamesDAO = new GamesDAO();
-    TeamsDAO teamsDAO = new TeamsDAO();
+//    PlayerDAO playerDAO = new PlayerDAO();
+//    AddressDAO addressDAO = new AddressDAO();
+//    GamesDAO gamesDAO = new GamesDAO();
+//    TeamsDAO teamsDAO = new TeamsDAO();
 
 
 
-    Player playerFromDatabase = playerDAO.getPlayerById(1);
-    System.out.println("Player fetched from database, first name: " + playerFromDatabase.getFirstName() + " , last name: " + playerFromDatabase.getLastName());
-        System.out.println(playerFromDatabase.getFirstName() + "'s address is: " + playerFromDatabase.getAddress().getAddress());
-    System.out.println("Size of list of players is: " + playerDAO.getAllPlayers().size());
+//    Player playerFromDatabase = playerDAO.getPlayerById(1);
+//    System.out.println("Player fetched from database, first name: " + playerFromDatabase.getFirstName() + " , last name: " + playerFromDatabase.getLastName());
+//        System.out.println(playerFromDatabase.getFirstName() + "'s address is: " + playerFromDatabase.getAddress().getAddress());
+//    System.out.println("Size of list of players is: " + playerDAO.getAllPlayers().size());
+//
+//
+//
+//    System.out.println("Address no.3 from database is: " + addressDAO.getAddressById(3).getAddress());
+//    System.out.println("Number of addresses in list is: " + addressDAO.getAllAddress().size());
+//
+//    System.out.println("Number of games available to play: " + gamesDAO.getAllGames().size());
+//
+//        System.out.println("Number of players in list: " + playerDAO.getAllPlayers().size());
+//
+//
+//
+//        Player playerToUpdate;
+//        playerToUpdate = playerDAO.getPlayerById(1);
+//        System.out.println("Player to update name: " + playerToUpdate.getFirstName());
+//        playerToUpdate.setGame(gamesDAO.getGameById(3));
+//        playerToUpdate.setTeam(teamsDAO.getTeamById(5));
+//        playerDAO.updatePlayer(playerToUpdate);
+//
+//        System.out.println("Team 3: " + teamsDAO.getTeamById(3).getName());
+//        System.out.println("Player updated, player team: " + playerToUpdate.getTeam().getName());
+//        System.out.println("Player updated, game played: " + playerToUpdate.getGame().getGameName());
 
 
+        TabPane tabPane = new TabPane();
+        Tab teamTab = new Tab("Teams", new TeamFX().getView());
+        Tab playerTab = new Tab("Players");
+        Tab gameTab = new Tab("Games");
+        Tab matchTab = new Tab("Matches");
 
-    System.out.println("Address no.3 from database is: " + addressDAO.getAddressById(3).getAddress());
-    System.out.println("Number of addresses in list is: " + addressDAO.getAllAddress().size());
-
-    System.out.println("Number of games available to play: " + gamesDAO.getAllGames().size());
-
-        System.out.println("Number of players in list: " + playerDAO.getAllPlayers().size());
-
-
-
-        Player playerToUpdate;
-        playerToUpdate = playerDAO.getPlayerById(1);
-        System.out.println("Player to update name: " + playerToUpdate.getFirstName());
-        playerToUpdate.setGame(gamesDAO.getGameById(3));
-        playerToUpdate.setTeam(teamsDAO.getTeamById(5));
-        playerDAO.updatePlayer(playerToUpdate);
-
-        System.out.println("Team 3: " + teamsDAO.getTeamById(3).getName());
-        System.out.println("Player updated, player team: " + playerToUpdate.getTeam().getName());
-        System.out.println("Player updated, game played: " + playerToUpdate.getGame().getGameName());
+        tabPane.getTabs().addAll(teamTab, playerTab, gameTab, matchTab);
 
 
-
-
-
-        AnchorPane root = new AnchorPane();
-        Scene scene = new Scene(root, 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(tabPane, 800, 600);
+        stage.setTitle("Piper Games");
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/gruppcadettsplitterpipergames/view/TeamFX.java
+++ src/main/java/com/example/gruppcadettsplitterpipergames/view/TeamFX.java
@@ -1,4 +1,100 @@
 package com.example.gruppcadettsplitterpipergames.view;
 
+import com.example.gruppcadettsplitterpipergames.DAO.TeamsDAO;
+import com.example.gruppcadettsplitterpipergames.entities.Game;
+import com.example.gruppcadettsplitterpipergames.entities.Team;
+import javafx.beans.Observable;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
+import java.sql.*;
+
 public class TeamFX {
+
+    private BorderPane view;
+    private TeamsDAO teamsDAO;
+    private ObservableList<Team> teamList;
+
+
+    public TeamFX() {
+        teamsDAO = new TeamsDAO();
+        view = new BorderPane();
+        teamList = FXCollections.observableArrayList();
+
+        HBox Hbuttons = new HBox();
+        VBox buttons = new VBox(20);
+        HBox labels = new HBox(500);
+
+        Button btnAdd = new Button("Add new team");
+        btnAdd.setPrefSize(200, 50);
+        Button btnUpdate = new Button("Update team");
+        btnUpdate.setPrefSize(200, 50);
+        Button btnDelete = new Button("Delete Team");
+        btnDelete.setPrefSize(200, 50);
+        Button btnRead = new Button("Search");
+        btnRead.setPrefSize(200, 50);
+        Hbuttons.getChildren().addAll(btnAdd, btnUpdate, btnDelete, btnRead);
+        view.setBottom(Hbuttons);
+//        Hbuttons.setAlignment(Pos.BOTTOM_CENTER);
+        //Hbuttons.setAlignment(Pos.CENTER_RIGHT);
+        Hbuttons.setPadding(new Insets(0, 0, 0, 0));
+
+        view.setLeft(labels);
+        labels.setAlignment(Pos.TOP_LEFT);
+
+        TableView<Team> teamTable = createTableView();
+        teamTable.setPrefWidth(800);
+        teamTable.setPrefHeight(350);
+        labels.setFillHeight(true);
+        labels.getChildren().add(teamTable);
+
+        loadTeamsFromDatabase(teamTable);
+
+    }
+
+    private TableView<Team> createTableView() {
+        TableView<Team> teamTable = new TableView<>();
+
+        // Lag-kolumn
+        TableColumn<Team, String> nameCol = new TableColumn<>("Team Name");
+        nameCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
+        nameCol.setPrefWidth(200);
+
+        // Spel-kolumn
+        TableColumn<Team, String> gameCol = new TableColumn<>("Game");
+        gameCol.setCellValueFactory(cellData -> {
+            Game game = cellData.getValue().getGame();
+            return new SimpleStringProperty(game != null ? game.getGameName() : "No game");
+        });
+        gameCol.setPrefWidth(200);
+
+        teamTable.getColumns().addAll(nameCol, gameCol);
+        return teamTable;
+    }
+
+    private void loadTeamsFromDatabase(TableView<Team> teamTable) {
+        teamList.setAll(teamsDAO.getAllTeams());
+        teamTable.setItems(teamList);
+    }
+
+
+    public BorderPane getView() {
+        return view;
+    }
+
+    public void setView(BorderPane view) {
+        this.view = view;
+    }
 }
