5b5a2ff0e5e656fbd6e87918077acafdd5a5a4fc
niklas-einarsson <n.einarsson@gmail.com>
2024-12-20 18:12:48 +0100

    Added function to filter teams by games
    Added method TeamsDAO.getTeamsByGameNames

--- src/main/java/com/example/gruppcadettsplitterpipergames/view/TeamFX.java
+++ src/main/java/com/example/gruppcadettsplitterpipergames/view/TeamFX.java
@@ -6,22 +6,39 @@ import com.example.gruppcadettsplitterpipergames.DAO.TeamsDAO;
 import com.example.gruppcadettsplitterpipergames.entities.Game;
 import com.example.gruppcadettsplitterpipergames.entities.Player;
 import com.example.gruppcadettsplitterpipergames.entities.Team;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import javafx.beans.Observable;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
+//import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TeamFX {
@@ -33,7 +50,7 @@ public class TeamFX {
     private ObservableList<Team> teamList;
 
 
-    public TeamFX() {
+    public TeamFX() throws FileNotFoundException {
         gameDAO = new GamesDAO();
         teamsDAO = new TeamsDAO();
         playerDAO = new PlayerDAO();
@@ -42,10 +59,9 @@ public class TeamFX {
         ListView<Player> playersInTeamList = new ListView();
 
 
-        HBox buttonsBox = new HBox();
+        HBox buttonsBox = new HBox(20);
         HBox tableBox = new HBox(20);
         VBox playerListBox = new VBox(5);
-
 
         Button btnAdd = new Button("Add new team");
         btnAdd.setPrefSize(200, 50);
@@ -53,43 +69,67 @@ public class TeamFX {
         btnUpdate.setPrefSize(200, 50);
         Button btnDelete = new Button("Delete Team");
         btnDelete.setPrefSize(200, 50);
-        Button btnRead = new Button("Search");
+        Button btnRead = new Button("Filter by Game");
         btnRead.setPrefSize(200, 50);
         buttonsBox.getChildren().addAll(btnAdd, btnUpdate, btnDelete, btnRead);
         view.setBottom(buttonsBox);
-        buttonsBox.setPadding(new Insets(0, 0, 0, 0));
+        buttonsBox.setPadding(new Insets(0, 0, 40, 40));
+
 
         view.setLeft(tableBox);
         tableBox.setAlignment(Pos.TOP_LEFT);
+        view.setStyle("-fx-background-color:silver");
 
         TableView<Team> teamTable = createTableView();
-        teamTable.setPrefWidth(500);
-        tableBox.setFillHeight(true);
+        teamTable.setPrefWidth(800);
+        tableBox.setFillHeight(false);
 
-        playersInTeamList.setMaxHeight(200);
+
+        ImageView logo = new ImageView(new Image(new FileInputStream("src/main/resources/logo.png")));
+        Circle logoClip = new Circle(50,50,40);
+        logo.setClip(logoClip);
+        logo.setPreserveRatio(true);
+        logo.setFitHeight(100);
+        Text title = new Text("Teams");
+        title.setStyle("-fx-font-size: 24;-fx-font-weight: bold;");
+        BorderPane header = new BorderPane();
+        header.setRight(logo);
+        header.setCenter(title);
+
+        view.setTop(header);
+
+        Label emptyPlayerListLabel = new Label("No players to view");
+        playersInTeamList.setPlaceholder(emptyPlayerListLabel);
+        playersInTeamList.setMaxHeight(250);
+        playersInTeamList.setMaxWidth(150);
+        playersInTeamList.setStyle("-fx-border-color: black; -fx-border-width: 2; -fx-border-radius: 2;");
+        teamTable.setStyle(" -fx-border-color: black; -fx-border-width: 2; -fx-border-radius: 2;");
+
         Label playerListLabel = new Label("Players in selected team");
         playerListLabel.setStyle("-fx-font-weight: bold");
         playerListBox.setAlignment(Pos.TOP_CENTER);
+        playerListBox.setPadding(new Insets(0, 0, 0, 0));
+
+
 
         playerListBox.getChildren().addAll(playerListLabel,playersInTeamList);
         tableBox.getChildren().addAll(teamTable, playerListBox);
+        teamTable.setPrefHeight(300);
+        playersInTeamList.setPrefHeight(teamTable.getPrefHeight());
 
         loadTeamsFromDatabase(teamTable);
 
-
-
-
-
+        // Visa lista med spelare när lag väljs i tabellen
         teamTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue != null) {
-                Team team = teamsDAO.getTeamById(newValue.getId()); // Hämta laget från databasen
-                List<Player> players = team.getPlayers(); // Få spelarna från databasen
+                Team team = teamsDAO.getTeamById(newValue.getId());
+                List<Player> players = team.getPlayers();
                 System.out.println("Players in team: " + players);
 
                 playersInTeamList.getItems().setAll(players);
             }
         });
-
+        // CellFactory för att visa förnamn + efternamn till listan
         playersInTeamList.setCellFactory(param -> new ListCell<Player>() {
             @Override
             protected void updateItem(Player player, boolean empty) {
@@ -147,7 +187,6 @@ public class TeamFX {
             GridPane gridPane = new GridPane();
             gridPane.setHgap(20);
             gridPane.setVgap(10);
-//            gridPane.setPadding(new Insets(20, 50, 10, 10));
 
             gridPane.add(new Label("Team Name"), 0, 0);
             gridPane.add(teamNameField, 1, 0);
@@ -194,40 +233,70 @@ public class TeamFX {
         btnRead.setOnAction(event -> {
             System.out.println("Read team");
 
-//            ComboBox<Game> gameCombo = new ComboBox<>();
-//            gameCombo.setPromptText("Select a game");
-//            gameCombo.getItems().addAll(gameDAO.getAllGames());
-//            view.setTop(gameCombo);
-
             ObservableList<Team> teamByGameList = FXCollections.observableArrayList();
+            Dialog<Boolean> dialog = new Dialog<>();
+            dialog.setTitle("Filter by game");
+            dialog.setHeaderText("Filter teams by games played");
 
+            Button buttonFilterByGame = new Button("Filter by selected games");
+            Button buttonCloseFilterByGame = new Button("Close");
 
-            String query = "SELECT t.team_id, t.team_name, t.game_id, g.name AS game_name " +
-                    "FROM teams t " +
-                    "LEFT JOIN games g ON t.game_id = g.id";
+            GridPane gridPane = new GridPane();
+            gridPane.setHgap(20);
+            gridPane.setVgap(10);
+            gridPane.add(buttonFilterByGame, 0, 4);
+            gridPane.add(buttonCloseFilterByGame, 0, 5);
 
+            dialog.getDialogPane().setContent(gridPane);
 
-            try (Connection connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/cadettsplitterspipergames", "root", "IOqw12");
-                 Statement statement = connection.createStatement();
-                 ResultSet resultSet = statement.executeQuery("SELECT team_id, team_name FROM teams")) {
-
-                while (resultSet.next()) {
-                    int teamId = resultSet.getInt("team_id");
-                    String teamName = resultSet.getString("team_name");
-
-                    // Skriv ut lagets ID och namn i konsolen
-                    System.out.println("Team ID: " + teamId + ", Team Name: " + teamName);
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-                System.out.println("Could not retrieve teams from the database.");
+            VBox checkBoxes = new VBox(10);
+            for (Game game : gameDAO.getAllGames()) {
+                CheckBox checkBox = new CheckBox(game.getGameName());
+                checkBox.setUserData(game);
+                checkBoxes.getChildren().add(checkBox);
             }
 
+            gridPane.add(checkBoxes, 0, 0);
 
-            teamTable.setItems(teamByGameList);
+            buttonFilterByGame.setOnAction(e -> {
 
+                List<Game> selectedGames = new ArrayList<>();
+
+                for (Node node : checkBoxes.getChildren()) {
+                    if (node instanceof CheckBox) {
+                        CheckBox checkBox = (CheckBox) node;
+                        if (checkBox.isSelected()) {
+                            Game game = (Game) checkBox.getUserData();
+                            selectedGames.add(game);
+                        }
+                    }
+                }
+
+                ObservableList<Team> teamsByGameList = FXCollections.observableArrayList();
+
+                if (selectedGames.isEmpty()) {
+                    teamsByGameList.addAll(teamsDAO.getAllTeams());
+                } else {
+
+                    List<Team> filteredTeams = teamsDAO.getTeamsByGameNames(selectedGames);
+                    teamsByGameList.addAll(filteredTeams);
+                }
+                teamTable.setItems(teamsByGameList);
+            });
+
+            buttonCloseFilterByGame.setOnAction(e -> {
+                System.out.println("Close");
+                dialog.setResult(Boolean.TRUE);
+                dialog.close();
+            });
+
+            dialog.showAndWait();
         });
+
+
+
+
+
 
         // DELETE
         btnDelete.setOnAction(event -> {
@@ -513,6 +582,9 @@ public class TeamFX {
         TableView<Team> teamTable = new TableView<>();
 
         //Id-kolumn
+        TableColumn<Team, Integer> idCol = new TableColumn<>("Team ID");
+        idCol.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getId()));
+        idCol.setPrefWidth(80);
 
 
         // Lag-kolumn
@@ -528,7 +600,7 @@ public class TeamFX {
         });
         gameCol.setPrefWidth(200);
 
-        teamTable.getColumns().addAll(nameCol, gameCol);
+        teamTable.getColumns().addAll(idCol,nameCol, gameCol);
         return teamTable;
     }
 
